b2bbc9ae2ce6532d0d26d4205d3d71f2533d86ae
kairia <8695024+kairia@users.noreply.github.com>
2022-03-21 14:20:59 +0800

    test

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { color } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.Text}>110719025 數位四 林耕祺</Text>
+      <Text style={styles.Text}>110719025 數位四 <Text style={styles.Text2}>林耕祺</Text></Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,6 +15,10 @@ const styles = StyleSheet.create({
   Text:{
     color:'red',
     fontSize:25,
+  },
+  Text2:{
+    color:'blue',
+    fontSize:35,
   },
   container: {
     flex: 1,
